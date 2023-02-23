896c4bce627bc02ae805df2816864e90cdee3df8
Harkirat <hs3188248@gmail.com>
2023-02-23 05:33:23 +0530

    js

---
+++ js Project/js/index.js
@@ -0,0 +1,29 @@
+
+
+console.log("hello")
+
+
+
+
+// create a function (function defination)
+
+function add_numbers(num_1 , num_2){
+
+var result = num_1 + num_2 ;
+
+return result;
+
+}
+
+
+
+
+// call a function (function call)
+
+var output1 = add_numbers(589,8542632);
+var output2= add_numbers(6,15);
+var output3 = add_numbers(56,4);
+
+console.log(output1)
+console.log(output2)
+console.log(output3)
