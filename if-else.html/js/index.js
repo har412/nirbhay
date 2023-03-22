bd9c4c8c76245802d9d86c91e2c73a70a31f0be4
Harkirat <hs3188248@gmail.com>
2023-03-23 04:12:15 +0530

    if else

---
+++ if-else.html/js/index.js
@@ -0,0 +1,13 @@
+
+
+var student_marks = 95;
+
+if(student_marks<60){
+    console.log("Please give exam again !")
+}
+else if(student_marks>90){
+console.log("You can get into top universities")
+}
+else{
+    console.log("You are qualified")
+}
